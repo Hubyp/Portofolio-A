6d278ee5c3dd6682497fd27f553d00b872cc9479
Hubyp <105743041+Hubyp@users.noreply.github.com>
2023-03-29 19:01:09 +0300

    Update _document.jsx

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -9,8 +9,8 @@ export default function MyDocument() {
             <meta name="googlebot-news" content="snippet" />
             <meta name="robots" content="follow" />
             <meta name="twitter:card" content="summary" />
-            <meta name="twitter:site" content="@JanjyTapYT" />
-            <meta name="twitter:creator" content="@JanjyTapYT" />
+            <meta name="twitter:site" content="@Name" />
+            <meta name="twitter:creator" content="@Name" />
             <meta name="twitter:title" content={`${config.siteMetadata.title}`} />
             <meta name="twitter:description" content={config.siteMetadata.description} />
             <meta property="og:type" content="website" />
@@ -20,7 +20,7 @@ export default function MyDocument() {
             <meta property="og:url" content={config.siteMetadata.url} />
             <meta name="theme-color" content={config.siteMetadata.themeColor} />
             <link rel="canonical" href={config.siteMetadata.url} />
-            <meta name="publisher" content="2022, JanjyTapYT" />
+            <meta name="publisher" content="2022, Name" />
             <meta name="author" content="me@janjytapyt.me" />
             <link href="https://pro.fontawesome.com/releases/v6.0.0-beta1/css/all.css" rel="stylesheet" />
         </Head>
